9f62e6ac267c5cebbaf2b22937bd34bf5e5c15a1
刘强 <GGBman@hotmail.com>
2020-06-08 10:47:47 +0800

    1234

---
+++ HelloLLLLL.Blog.Models/ImgJson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloLLLLL.Blog.Models
+{
+  public  class ImgJson
+    {
+        public int code { get; set; }
+        public List<string> data { get; set; }
+        public string msg { get; set; }
+    }
+}
---
+++ HelloLLLLL.Blog.Models/LoginInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloLLLLL.Blog.Models
+{
+    public class LoginInfo
+    {
+        public int Id { get; set; }
+        public string Ip { get; set; }
+        public string City { get; set; }
+        public string UserName { get; set; }
+        public string LoginDate { get; set; }
+        public string UserOS { get; set; }
+    }
+    public class LoginInfoData
+    {
+        public int code { get; set; }
+        public string msg { get; set; }
+        public int count { get; set; }
+        public List<LoginInfo> data { get; set; }
+    }
+}
